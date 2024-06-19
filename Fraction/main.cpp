693f5d1f051a7eb4382dfa9cbac113d09df44243
zheka <zheka@HOME-PC>
2024-06-19 18:54:02 +0300

    Comparison Operators DONE

--- Fraction/main.cpp
+++ Fraction/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+class Fraction;
+Fraction operator*(Fraction left, Fraction right);
+Fraction operator/(const Fraction left, const Fraction& right);
+
 class Fraction
 {
 	int integer;      //целая часть
@@ -65,10 +69,21 @@ public:
 		set_denominator(denominator);
 		cout << "Constructor: \t\t" << this << endl;
 	}
+	//operators: 
+	Fraction& operator*=(Fraction& other)
+	{
+		return *this = *this * other;
+	}
+
+	Fraction& operator/=(Fraction& other)
+	{
+		return *this = *this / other;
+	}
 	~Fraction()
 	{
 		cout << "Destructor:\t\t" << this << endl;
 	}
+
 	//       Methods:
 
 	Fraction& to_improper()
@@ -83,7 +98,29 @@ public:
 		numerator %= denominator;
 		return *this;
 	}
-
+	Fraction inverted()const
+	{
+		Fraction inverted = *this;
+		inverted.to_improper();
+		swap(inverted.numerator, inverted.denominator);
+		return inverted;
+	}
+	Fraction& reduce()
+	{
+		int more, less, rest;
+		if (numerator > denominator)more = numerator, less = denominator;
+		else less = numerator, more = denominator;
+		do
+		{
+			rest = more % less;
+			more = less;
+			less = rest;
+		} while (rest);
+		int GSD = more;// DSG - Greatest Common Divisor
+		numerator /= GSD;
+		denominator /= GSD;
+		return *this;
+	}
 	void print()const
 	{
 		if (integer)cout << integer;
@@ -96,24 +133,80 @@ public:
 		else if (integer == 0)cout << 0;
 		cout << endl;
 	}
-	
+
 };
-Fraction operator*(Fraction& left,Fraction& right)
+Fraction operator*(Fraction left, Fraction right)
 {
 	left.to_improper();
 	right.to_improper();
-	Fraction resoult;
+	/*Fraction resoult;
 	resoult.set_numerator(left.get_numerator()*right.get_numerator());
 	resoult.set_denominator(left.get_denominator()*right.get_denominator());
-	return resoult;
+	return resoult;*/
+
+
+	/*Fraction resoult
+	{
+		left.get_numerator()* right.get_numerator(),
+		left.get_denominator()* right.get_denominator()
+	};
+	resoult.to_proper();
+	return resoult;*/
+
+	return Fraction
+	(
+		left.get_numerator() * right.get_numerator(),
+		left.get_denominator() * right.get_denominator()
+
+	).to_proper().reduce();
 
 }
 
-//#define Constructors_check
+Fraction operator/(const Fraction left, const Fraction& right)
+{
+	return left * right.inverted();
+}
+//           Comparison operators
 
+bool operator==(Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	return left.get_numerator() * right.get_denominator() == right.get_numerator() * left.get_denominator();
+}
+bool operator!=(const Fraction& left,const Fraction& right)
+{
+	return !(left == right);
+}
+bool operator>(Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	return left.get_numerator() * right.get_denominator() >
+		right.get_numerator() * left.get_denominator();
+}
+bool operator<(Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	return left.get_numerator() * right.get_denominator() <
+		right.get_numerator() * left.get_denominator();
+}
+bool operator>=(const Fraction& left, const Fraction& right)
+{
+	return !(left < right);
+}
+bool operator<=(const Fraction& left, const Fraction& right)
+{
+	return !(left > right);
+}
+
+//#define Constructors_check
+//#define ARITHMETICAL_OPERATORS_CHECK
 void main()
 {
 	setlocale(LC_ALL, "");
+
 #ifdef Constructors_check
 	Fraction A;    //default constructor
 	A.print();
@@ -128,13 +221,24 @@ void main()
 	D.print();
 #endif // Constructors_check
 
+#ifdef ARITHMETICAL_OPERATORS_CHECK
 	Fraction A(2, 3, 4);
 	A.print();
-	Fraction B (3, 4, 5);
+	Fraction B(3, 4, 5);
 	B.print();
-	A.to_improper().print();
+	/*A.to_improper().print();
 	A.to_proper().print();
 
-	Fraction C = A * B;
-	C.print();
-} 
+	Fraction C = A / B;
+	C.print();*/
+
+	A *= B;
+	//cout << A << endl;
+	A.print();
+	A /= B;
+	A.print();
+#endif // ARITHMETICAL_OPERATORS_CHECK
+
+	cout << (Fraction(1, 2) <= Fraction(5, 11)) << endl;
+
+}
